da9dc7842824ef110f46ac7be591edd2d5f17ffd
ygvbhy <ygvbhy@naver.com>
2024-10-12 23:46:58 +0900

    React.memo add

--- src/components/List.js
+++ src/components/List.js
@@ -1,51 +1,57 @@
 import React from "react";
 
-export default function List({
-  todoData,
-  key,
-  id,
-  title,
-  completed,
-  setTodoData,
-  provided,
-  snapshot,
-}) {
-  const handleCompletedChange = (id) => {
-    const newTodoData = todoData.map((item) => {
-      item.id === id && (item.completed = !item.completed);
-      return item;
-    });
+const List = React.memo(
+  ({
+    todoData,
+    key,
+    id,
+    title,
+    completed,
+    setTodoData,
+    provided,
+    snapshot,
+  }) => {
+    const handleCompletedChange = (id) => {
+      const newTodoData = todoData.map((item) => {
+        item.id === id && (item.completed = !item.completed);
+        return item;
+      });
 
-    setTodoData(newTodoData);
-  };
+      setTodoData(newTodoData);
+    };
 
-  const handleClick = (id) => {
-    const newTodoData = todoData.filter((n) => n.id !== id);
-    setTodoData(newTodoData);
-  };
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${
-        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-    >
-      <div className="items-center">
-        <input
-          type="checkbox"
-          defaultChecked={completed}
-          onClick={() => handleCompletedChange(id)}
-        />
-        <span className={completed ? "line-through" : undefined}>{title}</span>
+    const handleClick = (id) => {
+      const newTodoData = todoData.filter((n) => n.id !== id);
+      setTodoData(newTodoData);
+    };
+    return (
+      <div
+        key={key}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+      >
+        <div className="items-center">
+          <input
+            type="checkbox"
+            defaultChecked={completed}
+            onClick={() => handleCompletedChange(id)}
+          />
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <div className="items-center ">
+          <button className="px-4 py-2" onClick={() => handleClick(id)}>
+            X
+          </button>
+        </div>
       </div>
-      <div className="items-center ">
-        <button className="px-4 py-2" onClick={() => handleClick(id)}>
-          X
-        </button>
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default List;
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import List from "./List";
 
-export default function Lists({ todoData, setTodoData }) {
+const Lists = React.memo(({ todoData, setTodoData }) => {
   const handleEnd = (result) => {
     if (!result.destination) return;
 
@@ -47,4 +47,6 @@ export default function Lists({ todoData, setTodoData }) {
       </DragDropContext>
     </div>
   );
-}
+});
+
+export default Lists;
